8e58faafb6d9907b9dec6f09bb3e2c5fd52f39a2
Thiago Reboredo <thiagoreboredo@gmail.com>
2026-02-27 17:38:35 -0300

    Ajuste no IngestAsync para enviar dados no formato para o Prometheus, com métricas específicas do monitoramento do sensor

--- src/IngestionService/Services/IngestionServiceImpl.cs
+++ src/IngestionService/Services/IngestionServiceImpl.cs
@@ -1,5 +1,6 @@
 using IngestionService.DTOs;
 using MassTransit;
+using Prometheus;
 using Shared.Messages;
 
 namespace IngestionService.Services;
@@ -8,6 +9,21 @@ public class IngestionServiceImpl : IIngestionService
 {
     private readonly IPublishEndpoint _publishEndpoint;
 
+    private static readonly Gauge SoilMoistureGauge = Metrics.CreateGauge(
+        "agrosolutions_soil_moisture_percent",
+        "Nivel de umidade do solo",
+        new GaugeConfiguration { LabelNames = new[] { "talhao_id", "talhao_name" } });
+
+    private static readonly Gauge TemperatureGauge = Metrics.CreateGauge(
+        "agrosolutions_temperature_celsius",
+        "Temperatura em graus Celsius",
+        new GaugeConfiguration { LabelNames = new[] { "talhao_id", "talhao_name" } });
+
+    private static readonly Gauge PrecipitationGauge = Metrics.CreateGauge(
+        "agrosolutions_precipitation_mm",
+        "Volume de precipitacao",
+        new GaugeConfiguration { LabelNames = new[] { "talhao_id", "talhao_name" } });
+
     public IngestionServiceImpl(IPublishEndpoint publishEndpoint)
     {
         _publishEndpoint = publishEndpoint;
@@ -15,6 +31,13 @@ public class IngestionServiceImpl : IIngestionService
 
     public async Task IngestAsync(SensorDataRequest request)
     {
+        var talhaoId = request.TalhaoId.ToString();
+        var talhaoName = $"Talhão {talhaoId[..8]}"; 
+
+        SoilMoistureGauge.WithLabels(talhaoId, talhaoName).Set(request.SoilMoisture);
+        TemperatureGauge.WithLabels(talhaoId, talhaoName).Set(request.Temperature);
+        PrecipitationGauge.WithLabels(talhaoId, talhaoName).Set(request.Precipitation);
+
         var message = new SensorDataMessage(
             request.TalhaoId,
             request.SoilMoisture,
